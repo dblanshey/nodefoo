b288f14194161400c92a5b295f60a4c69cc56e16
dblanshey <diana.blanshey@turner.com>
2015-06-02 14:04:28 -0400

    more info

--- hi.js
+++ hi.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
 
 app.get('/', function (req, res) {
   res.send('I love free food and free t-shirts!');
@@ -11,5 +12,10 @@ var server = app.listen(3000, function () {
   var port = server.address().port;
 
   console.log('Example app listening at http://%s:%s', host, port);
+
+  fs.writeFile('FOO.txt', 'Hello World! Your cows are delicious here at ' + host + ':' + port, function (err) {
+    if (err) throw err;
+
+  });
 
 });
