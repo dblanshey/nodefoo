53884f6b2b40daf454a855ab748e189181e0850a
dblanshey <dblanshey@users.noreply.github.com>
2015-06-03 15:21:24 -0400

    Update hi.js

--- hi.js
+++ hi.js
@@ -3,7 +3,7 @@ var app = express();
 var fs = require('fs');
 
 app.get('/', function (req, res) {
-  res.send('I love free food and free t-shirts!');
+  res.send('I love free food and free t-shirts! But even more so, I love cows! ');
 });
 
 var server = app.listen(3000, function () {
@@ -13,7 +13,7 @@ var server = app.listen(3000, function () {
 
   console.log('Example app listening at http://%s:%s', host, port);
 
-  fs.writeFile('FOO.txt', 'Hello World! Your cows are delicious here at ' + host + ':' + port, function (err) {
+  fs.writeFile('FOO.txt', 'Hello Brave New World! Your cows are delicious here at ' + host + ':' + port, function (err) {
     if (err) throw err;
 
   });
